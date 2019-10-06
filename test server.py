9781daa96517479ecaab7b1038f84f9e28d972df
xrlp <56208895+xrlplerl@users.noreply.github.com>
2019-10-05 21:20:04 -0300

    Delete test server.py

--- test server.py
+++
@@ -1,9 +0,0 @@
-import socket
-ip = '0.0.0.0'
-port = 4567
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind((ip,port))
-server.listen(5)
-data = server.accept()
-print(data)
-server.close()
