d0e0129c5857739e0d80b6cd931ce5059d0c55a6
xrlplerl <56208895+xrlplerl@users.noreply.github.com>
2020-12-29 09:45:22 -0300

    Esse script e para rodar em terminal
    Esse script e para rodar em terminal

---
+++ animes_download.py
@@ -0,0 +1,67 @@
+from bs4 import BeautifulSoup as bs
+import requests,os,webbrowser
+
+url = "https://animestorrent.com/?s="
+url2 = "https://animestorrent.com/category/lancamento/"
+
+def r(link):
+    return bs(requests.get(link).text,"html.parser")
+
+def cls1():
+    os.system("cls")
+
+def dow(url):
+    pg = r(url)
+    cont = (pg.find_all("ul",{"class":"buttons"}))[0]
+    link = (str(cont).split('"'))[5]
+    print(f"\n\x1b[0;97m{link}\x1b[0;37m")
+
+def pg1():
+    pg = r(url2)
+    c = (pg.find_all("div",{"class":"movies-list"}))[0]
+    c2 = c.find_all("div",{"class":"title"})
+    c3 = c.find_all("span",{"class":"imdb"})
+    n = 0
+    print("\n \x1b[0;93mUltimos lançamentos\n")
+    for t in c2:
+        print(f"\x1b[0;97m Anime: \x1b[1;96m{t.text} \x1b[0;97m{(str((c3[n]).text).split())[0]} \x1b[1;91m{(str((c3[n]).text).split())[1]}\x1b[0;37m")
+        n = n + 1
+
+def pg2():
+    name = str(input("\n \x1b[0;97mNome do anime a ser pesquisado: \x1b[0;37m"))
+    pg = r(f"{url+name}")
+    c = (pg.find_all("div",{"class":"movies-list"}))[0]
+    c2 = c.find_all("div",{"class":"title"})
+    if(len(c2) < 1):
+        print("\n\x1b[0;97m Não encontrado\n\x1b[0;37m")
+        exit()
+    else:
+        pass
+    c3 = c.find_all("span",{"class":"imdb"})
+    c4 = c.find_all("div",{"class":"image"})
+    n = 0
+    l = []
+    print("")
+    for t in c2:
+        try:
+            print(f"\x1b[0;92m [{n}]\x1b[0;97m Anime: \x1b[1;96m{t.text} \x1b[0;97m{(str((c3[n]).text).split())[0]} \x1b[1;91m{(str((c3[n]).text).split())[1]}\x1b[0;37m")
+            c5 = (c4[n].find_all("a"))
+            l.append((str(c5).split('"'))[1])
+            n = n + 1
+        except:
+            pass
+    try:
+        num = int(input("\n\x1b[0;97m Digite o numero referente ao anime que você quer baixar: \x1b[0;37m"))
+        if(num < (len(l))):
+            pass
+        else:
+            exit()
+    except:
+        print("\x1b[0;97m Deveria ter digitado o numero referente ao anime\x1b[0;37m")
+        exit()
+    
+    dow(str(l[num]))
+
+cls1()
+pg1()
+pg2()
