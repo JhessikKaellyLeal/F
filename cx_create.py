b0de300c6c85d09845a62416a1c1d50c40494bb8
xrlplerl <56208895+xrlplerl@users.noreply.github.com>
2021-09-14 14:11:08 -0300

    Script que auxilia na criação de executáveis
    Caso tenha duvida use: cx_create.py -h

---
+++ cx_create.py
@@ -0,0 +1,62 @@
+import sys,os
+
+bas = None
+if(len(sys.argv) >= 2):
+    if("-p" in sys.argv):
+        pak = (sys.argv[int(sys.argv.index("-p"))+1]).split(",")
+    else:
+        pak = []
+    if("-i" in sys.argv):
+        inc = (sys.argv[int(sys.argv.index("-i"))+1]).split(",")
+    else:
+        inc = []
+    if("-f" in sys.argv):
+        files = (sys.argv[int(sys.argv.index("-f"))+1]).split(",")
+    else:
+        files = []
+    if("-v" in sys.argv):
+        ver = sys.argv[int(sys.argv.index("-v"))+1]
+    else:
+        ver = "1"
+    if("-d" in sys.argv):
+        des = sys.argv[int(sys.argv.index("-d"))+1]
+    else:
+        des = ""
+    if("-w" in sys.argv):
+        if sys.platform == "win32":
+            bas = "Win32GUI"
+    else:
+        pass
+    if("-h" in sys.argv):
+        print("[+] ====> use -n para declarar o nome do aquivo.py\n[+] ====> -p para incluir as bibliotecas a serem importadas ex: tkinter,pyttsx3\n[+] ====> -i para includes\n[+] ====> -f para incluir arquivos\n[+] ====> -v para declar a versão do aplicação\n[+] ====> -d para declarar a versão\n[+] ====> -w caso queira usar interface grafica")
+        exit()
+    else:
+        pass
+    if("-n" in sys.argv):
+        nome = sys.argv[int(sys.argv.index("-n"))+1]
+    else:
+        print("[!] ]====> E necessario declarar o nome do arquivo com -n")
+        exit()
+else:
+    print("[+] ====> use -n para declarar o nome do aquivo.py\n[+] ====> -p para incluir as bibliotecas a serem importadas ex: tkinter,pyttsx3\n[+] ====> -i para includes\n[+] ====> -f para incluir arquivos\n[+] ====> -v para declar a versão do aplicação\n[+] ====> -d para declarar a versão\n[+] ====> -w caso queira usar interface grafica")
+    exit()
+
+topo = """
+from cx_Freeze import setup, Executable
+from cx_Freeze.dist import build_exe
+import sys
+"""
+for p in pak:
+    topo = topo + f"import {p}\n"
+
+base = f"""
+executable = [Executable('{nome}', base = '{bas}')]
+buildOptions = dict(packages = {pak},includes = {inc},include_files={files},excludes = [])
+setup(name = '{str(nome.replace(".py",""))}',version='{ver}',description='{des}',options=dict(build_exe = buildOptions),executables=executable)
+"""
+a = open("setup.py","w")
+a.write(topo+base)
+a.close()
+
+os.system("py setup.py build")
+os.system("del setup.py")
